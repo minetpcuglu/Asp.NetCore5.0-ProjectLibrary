617867a9b70d68f22a71f30d6fa01d64742f88fc
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-10-05 01:14:54 +0300

    05.10 / ViewComponents BookList /MT

---
+++ Asp.NetCore5.0-ProjectLibrary/ViewComponents/Book/BookList.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_ProjectLibrary.ViewComponents.Book
+{
+    
+    public class BookList:ViewComponent
+    {
+        BookManager bookManager = new BookManager(new EfBookDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var Value = bookManager.GetList();
+            return View(Value);
+        }
+    }
+}
